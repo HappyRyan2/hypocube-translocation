1e4c5f97d51be2e697c6eda55567235c5282b37e
HappyRyan2 <47163483+HappyRyan2@users.noreply.github.com>
2019-04-24 09:59:50 -0700

    Add files via upload

--- src/com/happyryan2/game/Game.java
+++ src/com/happyryan2/game/Game.java
@@ -11,9 +11,9 @@ import com.happyryan2.levels.*;
 
 public class Game {
 	private static boolean initialized = false;
-	public static String state = "play";
+	public static String state = "select-level";
 	public static List levelPacks = new ArrayList();
-	public static int packOpen = 1;
+	public static int packOpen = 0;
 	public static int levelOpen = 0;
 	public static float levelSize = 0;
 	public static float tileSize = 0;
@@ -22,6 +22,7 @@ public class Game {
 	public static boolean canClick = true;
 	public static boolean startingLevel = false;
 	public static Level currentLevel;
+	public static int transition = 0;
 	public static void run() {
 		if(!initialized) {
 			initialized = true;
@@ -67,5 +68,7 @@ public class Game {
 			Level level = (Level) pack.levels.get(levelOpen);
 			level.display(g);
 		}
+		g.setColor(new Color(255, 255, 255, transition));
+		g.fillRect(0, 0, 800, 800);
 	}
 }
