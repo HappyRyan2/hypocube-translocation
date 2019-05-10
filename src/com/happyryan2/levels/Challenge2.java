b109417d681b7d3e16a0e5d63711f502f09acc98
HappyRyan2 <47163483+HappyRyan2@users.noreply.github.com>
2019-05-10 10:37:17 -0700

    Add files via upload

--- src/com/happyryan2/levels/Challenge2.java
+++ src/com/happyryan2/levels/Challenge2.java
@@ -7,10 +7,14 @@ public class Challenge2 extends Level {
 	public Challenge2() {
         super.content.add(new Goal(2, 0));
         super.content.add(new Goal(0, 2));
+		super.content.add(new Retractor(0, 0, "down"));
         super.content.add(new Retractor(1, 1, "down"));
+        super.content.add(new Retractor(4, 0, "left"));
         super.content.add(new Retractor(3, 1, "left"));
         super.content.add(new Retractor(1, 3, "right"));
+        super.content.add(new Retractor(0, 4, "right"));
         super.content.add(new Retractor(3, 3, "up"));
+        super.content.add(new Retractor(4, 4, "up"));
         super.content.add(new Player(4, 2));
         super.content.add(new Player(2, 4));
 	}
