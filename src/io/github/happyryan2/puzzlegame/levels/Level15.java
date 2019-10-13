9e55a24f512eee6f95b6e95b200febbe78dbbab4
HappyRyan2 <happyryan2@hotmail.com>
2019-10-13 12:59:51 -0700

    added level 15, changed 8, 23-25, fixed bug

--- src/io/github/happyryan2/puzzlegame/levels/Level15.java
+++ src/io/github/happyryan2/puzzlegame/levels/Level15.java
@@ -10,5 +10,22 @@ public class Level15 extends Level {
 		super.requirements.add(13);
 		super.x = 0;
 		super.y = 800;
+
+		// super.content.add(new LongExtender(0, 2, "right", false));
+		// super.content.add(new LongExtender(8, 2, "left", false));
+		// super.content.add(new Wall(8, 0));
+		// super.content.add(new Wall(2, 2));
+		// super.content.add(new Wall(8, 4));
+		// super.content.add(new Goal(7, 4));
+		// super.content.add(new Player(5, 4));
+		// super.content.add(new Extender(4, 2, "right", false));
+		// super.content.add(new Retractor(4, 1, "down", false));
+		
+		super.content.add(new Player(1, 3));
+		super.content.add(new Goal(3, 3));
+		super.content.add(new Retractor(1, 1, "down", false));
+		super.content.add(new LongExtender(4, 2, "left", false));
+		super.content.add(new Wall(4, 0));
+		super.content.add(new LongExtender(0, 2, "right", false));
 	}
 }
--- src/io/github/happyryan2/puzzlegame/levels/Level23.java
+++ src/io/github/happyryan2/puzzlegame/levels/Level23.java
@@ -10,13 +10,29 @@ public class Level23 extends Level {
 		super.id = 23;
 		super.requirements.add(22);
 
-		super.content.add(new Retractor(2, 4, "down", false));
-		super.content.add(new Retractor(2, 5, "left", false));
-		super.content.add(new Goal(1, 1));
-		super.content.add(new Player(1, 5));
-		super.content.add(new Wall(3, 5));
-		super.content.add(new Wall(3, 6));
-		super.content.add(new LongExtender(1, 6, "up", true));
-		super.content.add(new Wall(2, 6));
+		super.content.add(new LongExtender(0, 2, "right", false));
+		super.content.add(new Player(1, 2));
+		super.content.add(new Goal(8, 2));
+		super.content.add(new Retractor(4, 2, "right", false));
+		super.content.add(new Retractor(6, 2, "left", false));
+		super.content.add(new Retractor(5, 3, "up", true));
+		super.content.add(new Wall(0, 0));
+		super.content.add(new Wall(1, 0));
+		super.content.add(new Wall(2, 0));
+		super.content.add(new Wall(3, 0));
+		super.content.add(new Wall(8, 0));
+		super.content.add(new Wall(7, 0));
+		super.content.add(new Wall(0, 1));
+		super.content.add(new Wall(1, 1));
+		super.content.add(new Wall(2, 1));
+		super.content.add(new Wall(3, 1));
+		super.content.add(new Wall(8, 1));
+		super.content.add(new Wall(7, 1));
+		super.content.add(new Wall(8, 3));
+		super.content.add(new Wall(7, 3));
+		super.content.add(new Wall(3, 3));
+		super.content.add(new Wall(2, 3));
+		super.content.add(new Wall(1, 3));
+		super.content.add(new Wall(0, 3));
 	}
 }
