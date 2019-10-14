356cadbd42c30095cbad08eb72dcbd35be4ad59e
HappyRyan2 <47163483+HappyRyan2@users.noreply.github.com>
2019-10-14 14:08:52 -0700

    Add files via upload

--- src/io/github/happyryan2/puzzlegame/levels/Level7.java
+++ src/io/github/happyryan2/puzzlegame/levels/Level7.java
@@ -9,17 +9,14 @@ public class Level7 extends Level {
 		super.requirements.add(6);
 		super.x = 400;
 		super.y = 400;
-
-		super.content.add(new Retractor(1, 3, "right", false));
-		super.content.add(new Retractor(3, 3, "left", false));
-		super.content.add(new Retractor(2, 2, "down", false));
-		super.content.add(new Wall(3, 4));
-		super.content.add(new Wall(4, 4));
-		super.content.add(new Player(3, 1));
-		super.content.add(new Goal(2, 1));
-		super.content.add(new Wall(0, 0));
-		super.content.add(new Wall(0, 1));
-		super.content.add(new Wall(4, 0));
-		super.content.add(new Wall(4, 1));
+		
+		super.content.add(new Extender(2, 2, "left", false));
+		super.content.add(new Player(2, 1));
+		super.content.add(new Goal(3, 1));
+		super.content.add(new Wall(3, 2));
+		super.content.add(new Wall(3, 3));
+		super.content.add(new Retractor(1, 1, "down", false));
+		super.content.add(new Extender(0, 2, "right", false));
+		super.content.add(new Wall(2, 3));
 	}
 }
